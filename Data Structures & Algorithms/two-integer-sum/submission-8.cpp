692cd764e5b64ffb171debf381865b176b012b84
Adrien Beavers <adrienkbeavers@gmail.com>
2026-08-04 05:20:26 -0700

    Add: two-integer-sum - submission-8

---
+++ Data Structures & Algorithms/two-integer-sum/submission-8.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> seen;
+        vector<int> res{2,-1};
+
+        for(int i{0}; i < nums.size(); ++i){
+            int toFind = target - nums[i];
+            if(seen.contains(toFind)){
+                res[0] = seen[toFind];
+                res[1] = i;
+                return res;
+            }
+            seen[nums[i]] = i;
+        }
+        return res;
+    }
+};
