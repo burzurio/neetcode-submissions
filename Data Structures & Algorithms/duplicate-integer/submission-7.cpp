1415ed5e900abe91df86aa7fc564a96418752bf0
Adrien Beavers <adrienkbeavers@gmail.com>
2026-08-03 20:40:42 -0700

    Add: duplicate-integer - submission-7

---
+++ Data Structures & Algorithms/duplicate-integer/submission-7.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        unordered_map<int, int> seen;
+
+        for(int i{0}; i < nums.size(); ++i){
+            if(seen.contains(nums[i])){
+                return true;
+            }
+            seen[nums[i]] = i;
+        }
+        return false;     
+    }
+};
